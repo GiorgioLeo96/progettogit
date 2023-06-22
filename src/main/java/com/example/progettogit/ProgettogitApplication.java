5abd5012db7753dbe9a4a591371e6380db50bf32
Giorgio Leo <gio.leo96@gmail.com>
2023-06-22 14:03:05 +0200

    first commit

---
+++ src/main/java/com/example/progettogit/ProgettogitApplication.java
@@ -0,0 +1,13 @@
+package com.example.progettogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettogitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/progettogit/ProgettogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progettogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
